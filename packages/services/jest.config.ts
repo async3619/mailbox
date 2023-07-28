f3babeefd3a4bf8f8cbc1ff19a8eb5bb383ed33a
Sophia <me@sophia-dev.io>
2023-07-29 01:04:05 +0900

    add services package

---
+++ packages/services/jest.config.ts
@@ -0,0 +1,20 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+    moduleFileExtensions: ["js", "json", "ts"],
+    rootDir: ".",
+    testRegex: ".*\\.spec\\.ts$",
+    transform: {
+        "^.+\\.(t|j)s$": "ts-jest",
+    },
+    collectCoverage: true,
+    collectCoverageFrom: ["./src/**/*.ts"],
+    coverageDirectory: "./coverage",
+    testEnvironment: "node",
+    roots: ["<rootDir>"],
+    modulePaths: ["."], // <-- This will be set to 'baseUrl' value
+    coveragePathIgnorePatterns: ["<rootDir>/src/fixtures/", "<rootDir>/src/index.ts"],
+    passWithNoTests: true,
+};
+
+module.exports = jestConfig;
